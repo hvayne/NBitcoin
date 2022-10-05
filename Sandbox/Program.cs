806f6b4cdbd023638c8c50822a403eff73f23fd0
Hannity Vayne <hvayne@pm.me>
2022-10-06 01:34:25 +0400

    generate mnemo

---
+++ Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using NBitcoin;
+
+namespace Sandbox
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.TwentyFour);
+			Console.WriteLine(mnemo);
+			Console.ReadKey();
+		}
+	}
+}
