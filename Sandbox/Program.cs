51872a26ffc40d177b87e10739de51da9f5af2eb
Hannity Vayne <hvayne@pm.me>
2022-10-06 05:58:17 +0400

    mnemonic to pk decimal

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,5 @@
-﻿using NBitcoin;
+﻿using System.Numerics;
+using NBitcoin;
 
 namespace Sandbox
 {
@@ -6,9 +7,29 @@ namespace Sandbox
 	{
 		static void Main()
 		{
-			Mnemonic mnemo = new Mnemonic(Wordlist.English, WordCount.TwentyFour);
-			Console.WriteLine(mnemo);
+			Mnemonic mnemonic = new Mnemonic("elbow silk cost label swamp " +
+				"exist throw fiction stamp aerobic hamster melt length " +
+				"spoon step rhythm print glass box rigid album burden" +
+				" ripple wagon", Wordlist.English);
+
+			ExtKey hdRoot = mnemonic.DeriveExtKey();
+
+			var keyPath = new KeyPath("44'/529'/0'/0/0");
+
+			ExtKey wallet = hdRoot.Derive(keyPath);
+
+			Key pk = wallet.PrivateKey;
+
+			BigInteger pk_dec = new(pk.ToBytes(), true, true);
+			Console.WriteLine($"base64 key = {Convert.ToBase64String(pk.ToBytes())}");
+			Console.WriteLine($"pk hex = {pk.ToHex()}");
+			Console.WriteLine($"pk decimal = {pk_dec}");
+
 			Console.ReadKey();
+		}
+		static string GetHex(byte[] bytes)
+		{
+			return BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
 		}
 	}
 }
